9e30409fe44b1c8ff441615c7963aa6656dc9a1b
Davide <davide.costantini@infomentum.co.uk>
2016-01-13 06:41:38 +0000

    first commit

---
+++ worker.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#########################################################
+LocalDir=""
+#########################################################
+
+iterate () {
+    cat logs_list.txt | while read l
+    do
+        IFS=':' read -a arr <<< "${l}"
+        host=${arr[0]}
+        port=${arr[1]}
+        user=${arr[2]}
+        key=${arr[3]}
+        log_path=${arr[4]}
+
+
+        #Skip on comments
+        if [ "${l:0:1}" == "#" ];then
+            continue
+        fi
+
+
+        #check user
+        if [ "$user" == "" ]; then
+           user="root"
+        fi
+
+        #check user
+        if [ "$port" == "" ]; then
+           port=22
+        fi
+
+
+        echo "Storing KEY for host $host"
+
+        ssh-keyscan -p $port -H $host >> ~/.ssh/known_hosts
+
+        get_log $host $port $user $key $log_path
+
+        echo "---------------------------------------------"
+
+    done
+}
+
+
+get_log () {
+    echo "Getting log from host ${1} using port ${2} and user ${3}"
+    if [ "$key" != "" ];then
+        echo "Using key: $4"
+        scp -i $4 -P $2 $3@$1:$log_path $LocalDir/basename($log_path) &> log_$1
+    else
+        scp -P $2 $3@$1:$log_path $LocalDir/basename($log_path) &> log_$1
+    fi
+}
+
+
+#init------
+echo "Starting `date -u`"
+iterate()
+echo "Completed - `date -u`"
